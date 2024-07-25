361111106cb6bb22acc6f755c2c3fe2fb985bb14
lipatov.st <lipatov.st@skaarena.ru>
2024-07-25 10:57:58 +0300

    Add files

--- regenerate.bat
+++ regenerate.bat
@@ -1,10 +1,10 @@
-@chcp 65001
+@chcp 65001 > NUL
 @set OLDDIR=%CD%
 @cd ..\index_generate
 @generate_html.py
 @cd %OLDDIR%
 @git add .
 @git commit -m "Add files"
-@git st
+::@git st
 @git push
 @pause
